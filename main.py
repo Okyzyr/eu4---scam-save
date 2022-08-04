49e1dbbd870aafd8a730cdf80ac3bafe1dc90e18
kcukierski@gmail.com <Liteon321?>
2022-08-04 18:12:43 +0200

    dodano zdialajacy guzik, zaciaga dane z pliku

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
 import datetime
 import os
 import shutil
-import sys
 import tkinter as tk
-from sys import platform
 from tkinter import ttk, Label
 from tkinter.messagebox import showinfo
 from tkinter.filedialog import askopenfilename
@@ -15,24 +13,33 @@ time = datetime.datetime.now()
 data = time.strftime("%m.%d-%H.%M.%S")
 
 
-def syschceck():
-    if platform == "linux" or platform == "linux2":
-        sys.exit()
-    elif platform == "darwin":
-        path = linecache.getline(r'cfg.txt', 2).strip()
-        return path
-    elif platform == "win32":
-        path = linecache.getline(r'cfg.txt', 4).strip()
-        return path
+def open_file_chooser():
+    filename = askopenfilename()
+    with open('cfg.txt', 'w') as file:
+        file.writelines(filename)
 
 
-path = syschceck()
+def file_path():
+    path = linecache.getline(r'cfg.txt', 1).strip()
+    split = path.rindex('/')
+    path = path[:split + 1]
+    return path
+
+def file_name():
+    file_name = linecache.getline(r'cfg.txt', 1).strip()
+    split = file_name.rindex('/')
+    file_name = file_name[split + 1:]
+    print(file_name)
+    return file_name
+
+path = file_path()
+file_name = file_name()
 
 dir_list = os.listdir(path)
 
 
 def backup():
-    file = None
+    file = file_name
     # info = input("Podaj krótkie info do pliku: ")
     if "backup" in dir_list:
         pass
@@ -40,10 +47,6 @@ def backup():
         folder_name = "backup"
         full_path = os.path.join(path, folder_name)
         os.mkdir(full_path)
-
-    for i in range(len(dir_list)):
-        if dir_list[i][-3:len(dir_list[i])] == "eu4" and len(dir_list[i]) == 5:
-            file = dir_list[i]
 
     src_path = path + file
     dst_path = path + "/backup/" + data + " (" + "CPY" + ") " + file
@@ -60,7 +63,7 @@ for i in range(len(dir_list)):
         plik = dir_list[i]
 
 root.title("EU4 Ironman Save Backup")
-root.geometry('500x130+50+50')
+root.geometry('500x150+50+50')
 root.resizable(True, True)
 root.iconbitmap('EU.ico')
 
@@ -87,4 +90,8 @@ backup_button.grid(row=3, column=0)
 
 label2 = Label(root, text='ścieżka zapisu save: \n' + path)
 label2.grid(row=4, column=0)
+
+btn_open = ttk.Button(root, text="Open", command=open_file_chooser)
+btn_open.grid(row=6, column=0)
+
 root.mainloop()
